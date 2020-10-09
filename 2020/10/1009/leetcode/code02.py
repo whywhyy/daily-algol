b649ce1d5238f242f1bea611e8d96dcf30114cb7
whywhyy <kangsan5240@naver.com>
2020-10-10 01:15:52 +0900

    ADD 1009

---
+++ 2020/10/1009/leetcode/code02.py
@@ -0,0 +1,26 @@
+# awesome
+class Codec:
+    def serialize(self, root):
+        """
+        Encodes a tree to a single string.
+        """
+        def postorder(root):
+            return postorder(root.left) + postorder(root.right) + [root.val] if root else []
+        return ' '.join(map(str, postorder(root)))
+
+    def deserialize(self, data):
+        """
+        Decodes your encoded data to tree.
+        """
+        def helper(lower = float('-inf'), upper = float('inf')):
+            if not data or data[-1] < lower or data[-1] > upper:
+                return None
+            
+            val = data.pop()
+            root = TreeNode(val)
+            root.right = helper(val, upper)
+            root.left = helper(lower, val)
+            return root
+        
+        data = [int(x) for x in data.split(' ') if x]
+        return helper()
